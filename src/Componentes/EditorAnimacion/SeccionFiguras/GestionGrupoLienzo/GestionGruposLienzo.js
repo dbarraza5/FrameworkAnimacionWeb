dada2291d9f709f95545e38d985757f372076da9
danielx25 <danielx-25@hotmail.com>
2022-07-26 20:08:21 -0400

    seleccion grupos en estructura de arbol

--- src/Componentes/EditorAnimacion/SeccionFiguras/GestionGrupoLienzo/GestionGruposLienzo.js
+++ src/Componentes/EditorAnimacion/SeccionFiguras/GestionGrupoLienzo/GestionGruposLienzo.js
@@ -5,11 +5,47 @@ function GestionGruposLienzo(props){
     const arbol=props.animacion.estructura_arbol_grupos()
 
     return (<div>
-        <TreeViewElement datos={arbol.nodes}/>
-        <button onClick={()=>{
-            const arbol = props.animacion.estructura_arbol_grupos()
-            console.log(arbol)
-        }}>vamos aver</button>
+        <div className="row">
+            <div className="col-6">
+                <div className="card">
+                    <div className="card-header">
+                        Estructura Arbol
+                    </div>
+                    <div className="card-body">
+                        <TreeViewElement datos={arbol.nodes}/>
+                    </div>
+                </div>
+            </div>
+            <div className="col-6">
+                <div className="form-check">
+                    <input className="form-check-input" type="radio" name="flexRadioDefault" id="flexRadioDefault1"/>
+                        <label className="form-check-label" htmlFor="flexRadioDefault1">
+                            Mover
+                        </label>
+                </div>
+                <div className="form-check">
+                    <input className="form-check-input" type="radio" name="flexRadioDefault" id="flexRadioDefault2"
+                           checked/>
+                        <label className="form-check-label" htmlFor="flexRadioDefault2">
+                            Rotar
+                        </label>
+                </div>
+                <div className="form-check">
+                    <input className="form-check-input" type="radio" name="flexRadioDefault" id="flexRadioDefault2"
+                           checked/>
+                    <label className="form-check-label" htmlFor="flexRadioDefault2">
+                        Aumentar
+                    </label>
+                </div>
+                <div className="form-check">
+                    <input className="form-check-input" type="radio" name="flexRadioDefault" id="flexRadioDefault2"
+                           checked/>
+                    <label className="form-check-label" htmlFor="flexRadioDefault2">
+                        Reducir
+                    </label>
+                </div>
+            </div>
+        </div>
     </div>)
 }
 
--- src/Componentes/TreeView/TreeViewElement.js
+++ src/Componentes/TreeView/TreeViewElement.js
@@ -3,31 +3,61 @@ import {useEffect, useState} from "react";
 
 function TreeViewElement(props){
     const data_tree = props.datos;
+    const [lista_seleccionados, setListaSeleccionados] = useState([])
+    const [nombre_nodo, setNombreNodo] = useState("")
 
-    const click_nodo=(event)=>{
+    const click_nodo=(event, nombre_grupo)=>{
         event.target.parentElement.querySelector(".nested").classList.toggle("active");
         event.target.classList.toggle("caret-down");
+        setNombreNodo(nombre_grupo)
+    }
+
+    const click_leaf=(event, nombre_grupo)=>{
+        setNombreNodo(nombre_grupo)
+        console.log(nombre_grupo)
+    }
+
+    const seleccion_grupo=(e, nombre_grupo)=>{
+        const select = e.target.checked;
+        if(select){
+            setListaSeleccionados(lista_seleccionados.concat([nombre_grupo]))
+        }else{
+            setListaSeleccionados(lista_seleccionados.filter((nombre)=>nombre!==nombre_grupo))
+        }
     }
 
     const construir_arbol=(lista_nodos, first=false)=>{
         let lista_tag_nodo = []
         for (let i=0; i<lista_nodos.length; i++){
             const nodo = lista_nodos[i];
+            let style=null
+            if(nodo.text === nombre_nodo){
+                style = {color: "blue"}
+            }
             if("nodes" in nodo){
-                const tag_nodo = (<li><span className="caret" onClick={(e)=>click_nodo(e)}>{nodo.text}</span>
+                const tag_nodo = (<li>
+                    <span  style={style} className="caret" onClick={(e)=>click_nodo(e, nodo.text)} htmlFor="firstCheckbox">
+                    </span>
+                    <input className="form-check-input me-1" type="checkbox" value=""
+                           id={"checkbox-grupo"+nodo.text} onChange={(e)=>seleccion_grupo(e, nodo.text)}/>
+                    <label className="form-check-label" htmlFor={"checkbox-grupo"+nodo.text}>{nodo.text}</label>
+
                     {construir_arbol(nodo.nodes)}
                     </li>)
                 lista_tag_nodo.push(tag_nodo)
             }else{
-                lista_tag_nodo.push(<li>{nodo.text}</li>)
+                //lista_tag_nodo.push(<li style={style} onClick={(e)=>click_leaf(e,nodo.text)}>{nodo.text}</li>)
+                lista_tag_nodo.push(<li style={style} onClick={(e)=>click_leaf(e,nodo.text)}>
+                    <input className="form-check-input me-1" type="checkbox" value=""
+                           id={"checkbox-grupo"+nodo.text} onChange={(e)=>seleccion_grupo(e, nodo.text)}/>
+                    <label className="form-check-label" htmlFor={"checkbox-grupo"+nodo.text}>{nodo.text}</label>
+                </li>)
             }
         }
         return(<ul className={first?"active":"nested"}>
             {lista_tag_nodo}
         </ul>)
     }
-
-
 
     useEffect(() => {
     }, []);
