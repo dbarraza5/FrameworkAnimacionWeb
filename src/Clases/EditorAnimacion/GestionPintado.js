e1558a61058d2f5750cd7bf98226bac1d7d3639e
danielx25 <danielx-25@hotmail.com>
2023-06-30 16:26:49 -0400

    creacion del aparatado de pintar parte 7 - inicializacion completada

--- src/Clases/EditorAnimacion/GestionPintado.js
+++ src/Clases/EditorAnimacion/GestionPintado.js
@@ -1,12 +1,34 @@
-class GestionPintado{
+import OperacionesGrupo from "./OperacionesGrupo";
+
+class GestionPintado {
     grupo = null;
     grupo_copia = null;
+
     constructor() {
     }
 
-    inicializarGrupo(grupo_){
+    inicializarGrupo(grupo_) {
         this.grupo = grupo_;
         this.grupo_copia = JSON.parse(JSON.stringify(grupo_))
+        let centro_grupo = OperacionesGrupo.calcularCentroGruposSeleccionados([this.grupo_copia])
+        OperacionesGrupo.moverGrupo(this.grupo_copia, grupo_, centro_grupo.centro_x,
+            centro_grupo.centro_y, 300, 300)
+        const grupo_aux = JSON.parse(JSON.stringify(this.grupo_copia))
+        centro_grupo = OperacionesGrupo.calcularCentroGruposSeleccionados(
+            [grupo_aux])
+        this.inflar_grupo(this.grupo_copia, grupo_aux,0.5, centro_grupo.centro_x,
+            centro_grupo.centro_y)
+    }
+
+    inflar_grupo(grupo_, grupo_copia_, porcentaje, centrox, centroy) {
+        for (let j = 0; j < grupo_copia_.lista_figuras.length; j++) {
+            let figura = grupo_.lista_figuras[j];
+            let f_copia = grupo_copia_.lista_figuras[j];
+
+            OperacionesGrupo.inflar_figura(figura, f_copia, grupo_, centrox,
+                centroy, porcentaje)
+
+        }
     }
 };
 
