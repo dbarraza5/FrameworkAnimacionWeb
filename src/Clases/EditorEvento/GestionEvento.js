b0ee6b80719efee19c35b9fd623d7c0ccaa39d21
Daniel Barraza <dbarraza@visiion.cl>
2025-04-28 21:51:04 -0400

    procesamiento de movimiento para imprimir parte2 tratando de hacer mover grupo

--- src/Clases/EditorEvento/GestionEvento.js
+++ src/Clases/EditorEvento/GestionEvento.js
@@ -22,6 +22,7 @@ class GestionEvento{
 
     tiempo=new Tiempo();
 
+    movimientos_grupos = {};
     copia_grupos = [];
     gestion_eventos = null;
     gestion_grupos = null;
@@ -36,6 +37,14 @@ class GestionEvento{
         this.configuracion_lienzo = new ConfiguracionLienzo();
         this.imprimir_animacion = new ImprimirAnimacion(this.imprimir_animacion,this.configuracion_lienzo, this.id_canvas);
     }
+
+    reseteoMovGrupos(){
+        for(let i=0; i<this.gestion_grupos_originales.grupos_figuras.length; i++){
+            const grupo_ = this.gestion_grupos_originales.grupos_figuras[i];
+            this.movimientos_grupos[grupo_.nombre] = {x: 0, y:0};
+        }
+    }
+
     inicializar(lista_evento, grupos){
         //this.grupos = grupos;
         this.gestion_grupos.grupos_figuras=grupos;
@@ -50,6 +59,7 @@ class GestionEvento{
         console.log(this.gestion_grupos.grupos_figuras);
         this.imprimirEventos();
         this.tiempo.modPasivo();
+        this.reseteoMovGrupos();
     }
 
     iniciarRelojes(){
@@ -59,14 +69,15 @@ class GestionEvento{
 
     procesandoEventos(){
         tiempo_universal=performance.now();
-        const tiempo = this.tiempo.cronometroC();
+        const tiempo = this.tiempo.cronometroC()/1000;
 
-        for(let i=0; i<this.eventos.length; i++){
-            for(let j=0; j<this.eventos[i].evento.objetos.length; j++){
-                this.eventos[i].evento.objetos[j].x_mov =0;
-                this.eventos[i].evento.objetos[j].y_mov =0;
-            }
-        }
+        // for(let i=0; i<this.eventos.length; i++){
+        //     for(let j=0; j<this.eventos[i].evento.objetos.length; j++){
+        //         this.eventos[i].evento.objetos[j].x_mov =0;
+        //         this.eventos[i].evento.objetos[j].y_mov =0;
+        //     }
+        // }
+        this.reseteoMovGrupos();
 
         for(let i=0; i<this.eventos.length; i++){
             if(this.eventos[i].evento.tiempo_inicio>=tiempo
@@ -75,7 +86,7 @@ class GestionEvento{
                     this.eventos[i].inicio=true;
                     this.eventos[i].tiempo.modPasivo();
                 }
-                const tiempo_evento = this.eventos[i].tiempo.cronometroC();
+                const tiempo_evento = this.eventos[i].tiempo.cronometroC()/1000;
 
                 for(let j=0; j<this.eventos[i].movimientos.length; j++){
                     const movimiento = this.eventos[i].movimientos[j];
@@ -84,13 +95,17 @@ class GestionEvento{
                     const resultado = GestionMovimientos.movimientoGrupo(tiempo_evento, tipo, datos);
                     const x = resultado.x;
                     const y = resultado.y;
-
+                    console.log("[1] MOV ${x}, ${y}");
                     for(let k=0; k<movimiento.ids_grupos.length; k++){
                         const id_grupo = movimiento.ids_grupos[k];
                         const indice = this.eventos[i].objetos.findIndex((objeto)=>objeto.id_objeto===id_grupo);
-                        if(indice > 0){
-                            this.eventos[i].objetos[indice].x_mov+=x;
-                            this.eventos[i].objetos[indice].y_mov+=y;
+                        console.log("indice: ".indice);
+                        if(indice >= 0){
+                            // this.eventos[i].objetos[indice].x_mov+=x;
+                            // this.eventos[i].objetos[indice].y_mov+=y;
+                            console.log("[2] MOV ${x}, ${y}");
+                            this.movimientos_grupos[id_grupo].x+=x;
+                            this.movimientos_grupos[id_grupo].y+=y;
                         }
                     }
                 }
@@ -105,7 +120,14 @@ class GestionEvento{
                 // this.eventos[i].evento.objetos[j].y_mov =0;
             }
         }
-        console.log("tiempo: "+tiempo_universal);
+        for(let i=0; i<this.gestion_grupos.grupos_figuras.length; i++){
+            const grupo_ = this.gestion_grupos_originales.grupos_figuras[i];
+            const x = grupo_.cx+this.movimientos_grupos[grupo_.nombre].x;
+            const y = grupo_.cy+this.movimientos_grupos[grupo_.nombre].y;
+            this.gestion_grupos.set_atributo_grupo(grupo_.nombre, "cx", x);
+            this.gestion_grupos.set_atributo_grupo(grupo_.nombre, "cy", y);
+        }
+        console.log("tiempo1: "+tiempo_universal);
     }
 
     movimientos(tiempo, lista_mov, objetos){
